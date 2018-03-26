ab94cb86a27d78247a9c2ec9ec21a67090eb4d1a
PawełW <pawel-wilgan@op.pl>
2018-03-26 17:44:45 +0200

    2

--- MMM/MyForm.h
+++ MMM/MyForm.h
@@ -39,6 +39,14 @@ namespace MMM {
 	private: System::Windows::Forms::RadioButton^  Triangle;
 	private: System::Windows::Forms::RadioButton^  Rectangel;
 	private: System::Windows::Forms::Label^  Tytu³;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::TextBox^  textBox3;
+	private: System::Windows::Forms::TextBox^  textBox4;
+	private: System::Windows::Forms::Label^  Ampituda;
+	private: System::Windows::Forms::Label^  Okres;
+	private: System::Windows::Forms::Label^  A1;
+	private: System::Windows::Forms::Label^  A2;
 
 
 
@@ -68,6 +76,14 @@ namespace MMM {
 			this->Triangle = (gcnew System::Windows::Forms::RadioButton());
 			this->Rectangel = (gcnew System::Windows::Forms::RadioButton());
 			this->Tytu³ = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->Ampituda = (gcnew System::Windows::Forms::Label());
+			this->Okres = (gcnew System::Windows::Forms::Label());
+			this->A1 = (gcnew System::Windows::Forms::Label());
+			this->A2 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// Start
@@ -76,6 +92,7 @@ namespace MMM {
 			this->Start->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
 			this->Start->Location = System::Drawing::Point(1128, 580);
+			this->Start->Margin = System::Windows::Forms::Padding(2, 4, 2, 4);
 			this->Start->Name = L"Start";
 			this->Start->Size = System::Drawing::Size(107, 40);
 			this->Start->TabIndex = 0;
@@ -89,6 +106,7 @@ namespace MMM {
 			this->Sinus->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
 			this->Sinus->Location = System::Drawing::Point(47, 105);
+			this->Sinus->Margin = System::Windows::Forms::Padding(2, 4, 2, 4);
 			this->Sinus->Name = L"Sinus";
 			this->Sinus->Size = System::Drawing::Size(292, 24);
 			this->Sinus->TabIndex = 1;
@@ -102,6 +120,7 @@ namespace MMM {
 			this->Triangle->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
 			this->Triangle->Location = System::Drawing::Point(47, 128);
+			this->Triangle->Margin = System::Windows::Forms::Padding(2, 4, 2, 4);
 			this->Triangle->Name = L"Triangle";
 			this->Triangle->Size = System::Drawing::Size(278, 24);
 			this->Triangle->TabIndex = 2;
@@ -115,6 +134,7 @@ namespace MMM {
 			this->Rectangel->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
 			this->Rectangel->Location = System::Drawing::Point(47, 151);
+			this->Rectangel->Margin = System::Windows::Forms::Padding(2, 4, 2, 4);
 			this->Rectangel->Name = L"Rectangel";
 			this->Rectangel->Size = System::Drawing::Size(287, 24);
 			this->Rectangel->TabIndex = 3;
@@ -128,24 +148,114 @@ namespace MMM {
 			this->Tytu³->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 26.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(238)));
 			this->Tytu³->Location = System::Drawing::Point(283, 31);
+			this->Tytu³->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->Tytu³->Name = L"Tytu³";
 			this->Tytu³->Size = System::Drawing::Size(736, 39);
 			this->Tytu³->TabIndex = 4;
 			this->Tytu³->Text = L"Metody Modelowania Matematycznego Projekt";
 			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(430, 134);
+			this->textBox1->Margin = System::Windows::Forms::Padding(2, 4, 2, 4);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 5;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(642, 134);
+			this->textBox2->Margin = System::Windows::Forms::Padding(2, 4, 2, 4);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->TabIndex = 6;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(860, 134);
+			this->textBox3->Margin = System::Windows::Forms::Padding(2, 4, 2, 4);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 20);
+			this->textBox3->TabIndex = 7;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(1063, 134);
+			this->textBox4->Margin = System::Windows::Forms::Padding(2, 4, 2, 4);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(100, 20);
+			this->textBox4->TabIndex = 8;
+			// 
+			// Ampituda
+			// 
+			this->Ampituda->AutoSize = true;
+			this->Ampituda->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->Ampituda->Location = System::Drawing::Point(418, 111);
+			this->Ampituda->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->Ampituda->Name = L"Ampituda";
+			this->Ampituda->Size = System::Drawing::Size(125, 18);
+			this->Ampituda->TabIndex = 9;
+			this->Ampituda->Text = L"Ampituda Sygna³u";
+			// 
+			// Okres
+			// 
+			this->Okres->AutoSize = true;
+			this->Okres->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->Okres->Location = System::Drawing::Point(638, 111);
+			this->Okres->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->Okres->Name = L"Okres";
+			this->Okres->Size = System::Drawing::Size(105, 18);
+			this->Okres->TabIndex = 10;
+			this->Okres->Text = L"Okres Sygna³u";
+			// 
+			// A1
+			// 
+			this->A1->AutoSize = true;
+			this->A1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->A1->Location = System::Drawing::Point(868, 111);
+			this->A1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->A1->Name = L"A1";
+			this->A1->Size = System::Drawing::Size(81, 18);
+			this->A1->TabIndex = 11;
+			this->A1->Text = L"Zwê¿ka A1";
+			// 
+			// A2
+			// 
+			this->A2->AutoSize = true;
+			this->A2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 11.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(238)));
+			this->A2->Location = System::Drawing::Point(1070, 111);
+			this->A2->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
+			this->A2->Name = L"A2";
+			this->A2->Size = System::Drawing::Size(81, 18);
+			this->A2->TabIndex = 12;
+			this->A2->Text = L"Zwê¿ka A2";
+			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1284, 641);
+			this->ClientSize = System::Drawing::Size(1283, 633);
+			this->Controls->Add(this->A2);
+			this->Controls->Add(this->A1);
+			this->Controls->Add(this->Okres);
+			this->Controls->Add(this->Ampituda);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->Tytu³);
 			this->Controls->Add(this->Rectangel);
 			this->Controls->Add(this->Triangle);
 			this->Controls->Add(this->Sinus);
 			this->Controls->Add(this->Start);
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->Margin = System::Windows::Forms::Padding(2, 4, 2, 4);
 			this->Name = L"MyForm";
-			this->Text = L"MyForm";
+			this->Text = L"MMM";
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
